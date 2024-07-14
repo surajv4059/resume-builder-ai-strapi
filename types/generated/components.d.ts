445c2a7330e5238faf61b224e1491bc24afd1134
Suraj Verma <suraj12.116@gmail.com>
2024-07-15 03:55:47 +0530

    completed

---
+++ types/generated/components.d.ts
@@ -0,0 +1,55 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'skills';
+    icon: 'apps';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    workSummary: Schema.Attribute.Text;
+  };
+}
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'education';
+    icon: 'book';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'skills.skills': SkillsSkills;
+      'experience.experience': ExperienceExperience;
+      'education.education': EducationEducation;
+    }
+  }
+}
